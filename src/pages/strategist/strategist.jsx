cb0b3ee4ae21f4712ecbfca0b70a7131cdcc139a
Hossam <hossam.zerouali@estudiantat.upc.edu>
2024-09-29 21:37:34 +0200

    update strategist css

--- src/pages/strategist/strategist.jsx
+++ src/pages/strategist/strategist.jsx
@@ -1,33 +1,41 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; // Importa Link para la navegación
 import Header from '../../components/header/header';
 import './strategist.css';
 
-const circuits = [
-    "abu dhabi",
-    "australia",
-    "austria",
-    "azerbaijan",
-    "bahrain",
-    "belgium",
-    "brazil",
-    "canada",
-    "china",
-    "hungary",
-    "imola",
-    "japan",
-    "mexico",
-    "miami",
-    "monaco",
-    "monza",
-    "netherlands",
-    "qatar",
-    "saudi arabia",
-    "singapore",
-    "spain",
-    "united kingdom",
-    "usa - austin",
-    "usa - vegas"
-];
+const circuitMap = {
+    "abu dhabi": "Abu Dhabi",
+    "australia": "Australia",
+    "austria": "Austria",
+    "azerbaijan": "Azerbaijan",
+    "bahrain": "Bahrain",
+    "belgium": "Bélgica",
+    "brazil": "Brasil",
+    "canada": "Canadá",
+    "china": "China",
+    "hungary": "Hungría",
+    "imola": "Imola",
+    "japan": "Japón",
+    "mexico": "México",
+    "miami": "Miami",
+    "monaco": "Mónaco",
+    "monza": "Monza",
+    "netherlands": "Países Bajos",
+    "qatar": "Catar",
+    "saudi arabia": "Arabia Saudita",
+    "singapore": "Singapur",
+    "spain": "España",
+    "united kingdom": "Reino Unido",
+    "usa - austin": "EE.UU. - Austin",
+    "usa - vegas": "EE.UU. - Las Vegas"
+};
+
+const methodMap = {
+    "ByCircuit": "Por Circuito",
+    "ByStintsData": "Por Datos de Stints"
+};
+
+const circuits = Object.keys(circuitMap);
 
 function Strategist() {
     const [tyreData, setTyreData] = useState({
@@ -62,14 +70,11 @@ function Strategist() {
                 })
             });
 
-            console.log('Response status:', response.status);
-
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
 
             const text = await response.text();
-            console.log('Raw response text:', text);
             setResponseText(text);
         } catch (error) {
             console.error('There was a problem with the fetch operation:', error.message);
@@ -79,105 +84,121 @@ function Strategist() {
     };
 
     return (
-        <div className="main-container">
+        <div>
             <Header />
-            <form onSubmit={handleSubmit}>
-                <h2>Tyre Data</h2>
-                <div className={tyreDataMethod === 'ByStintsData' ? '' : 'hidden'}>
-                    <label>Soft Time: </label>
-                    <input
-                        type="number"
-                        value={tyreData.soft.time}
-                        onChange={(e) => setTyreData({ ...tyreData, soft: { ...tyreData.soft, time: parseFloat(e.target.value) } })}
-                    />
-                </div>
-                <div className={tyreDataMethod === 'ByStintsData' ? '' : 'hidden'}>
-                    <label>Soft Loss: </label>
-                    <input
-                        type="number"
-                        value={tyreData.soft.loss}
-                        onChange={(e) => setTyreData({ ...tyreData, soft: { ...tyreData.soft, loss: parseFloat(e.target.value) } })}
-                    />
-                </div>
-                <div className={tyreDataMethod === 'ByStintsData' ? '' : 'hidden'}>
-                    <label>Medium Time: </label>
-                    <input
-                        type="number"
-                        value={tyreData.medium.time}
-                        onChange={(e) => setTyreData({ ...tyreData, medium: { ...tyreData.medium, time: parseFloat(e.target.value) } })}
-                    />
-                </div>
-                <div className={tyreDataMethod === 'ByStintsData' ? '' : 'hidden'}>
-                    <label>Medium Loss: </label>
-                    <input
-                        type="number"
-                        value={tyreData.medium.loss}
-                        onChange={(e) => setTyreData({ ...tyreData, medium: { ...tyreData.medium, loss: parseFloat(e.target.value) } })}
-                    />
-                </div>
-                <div className={tyreDataMethod === 'ByStintsData' ? '' : 'hidden'}>
-                    <label>Hard Time: </label>
-                    <input
-                        type="number"
-                        value={tyreData.hard.time}
-                        onChange={(e) => setTyreData({ ...tyreData, hard: { ...tyreData.hard, time: parseFloat(e.target.value) } })}
-                    />
-                </div>
-                <div className={tyreDataMethod === 'ByStintsData' ? '' : 'hidden'}>
-                    <label>Hard Loss: </label>
-                    <input
-                        type="number"
-                        value={tyreData.hard.loss}
-                        onChange={(e) => setTyreData({ ...tyreData, hard: { ...tyreData.hard, loss: parseFloat(e.target.value) } })}
-                    />
-                </div>
-                <div>
-                    <label>Circuit: </label>
-                    <select
-                        value={circuit}
-                        onChange={(e) => setCircuit(e.target.value)}
-                    >
-                        <option value="">Select a circuit</option>
-                        {circuits.map((circuit, index) => (
-                            <option key={index} value={circuit}>{circuit}</option>
+            <div className="strategist-container">
+                <h1>Formula 1 - Strategist</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="tyre-method-tabs">
+                        {Object.keys(methodMap).map((methodKey) => (
+                            <button
+                                key={methodKey}
+                                type="button"
+                                className={tyreDataMethod === methodKey ? 'active' : ''}
+                                onClick={() => setTyreDataMethod(methodKey)}
+                            >
+                                {methodMap[methodKey]} {/* Mostrar el nombre amigable */}
+                            </button>
                         ))}
-                    </select>
-                </div>
-                <div>
-                    <label>Laps: </label>
-                    <input
-                        type="number"
-                        value={laps}
-                        onChange={(e) => setLaps(parseInt(e.target.value))}
-                    />
-                </div>
-                <div>
-                    <label>Pit Time: </label>
-                    <input
-                        type="number"
-                        value={pitTime}
-                        onChange={(e) => setPitTime(parseInt(e.target.value))}
-                    />
-                </div>
-                <div>
-                    <label>Tyre Data Method: </label>
-                    <select
-                        value={tyreDataMethod}
-                        onChange={(e) => setTyreDataMethod(e.target.value)}
-                    >
-                        <option value="ByCircuit">ByCircuit</option>
-                        <option value="ByStintsData">ByStintsData</option>
-                    </select>
-                </div>
-                <button type="submit">Submit</button>
-            </form>
-            {loading && <p>Estamos calculando la estrategia...</p>}
-            {responseText && (
-                <div className="response-container">
-                    <h3>Response Data</h3>
-                    <pre>{responseText}</pre>
-                </div>
-            )}
+                    </div>
+                    <div className="input-frame">
+                        {tyreDataMethod === 'ByStintsData' && (
+                            <>
+                                <div className="input-group">
+                                    <label>Soft Time: </label>
+                                    <input
+                                        type="number"
+                                        value={tyreData.soft.time}
+                                        onChange={(e) => setTyreData({ ...tyreData, soft: { ...tyreData.soft, time: parseFloat(e.target.value) } })}
+                                    />
+                                </div>
+                                <div className="input-group">
+                                    <label>Soft Loss: </label>
+                                    <input
+                                        type="number"
+                                        value={tyreData.soft.loss}
+                                        onChange={(e) => setTyreData({ ...tyreData, soft: { ...tyreData.soft, loss: parseFloat(e.target.value) } })}
+                                    />
+                                </div>
+                                <div className="input-group">
+                                    <label>Medium Time: </label>
+                                    <input
+                                        type="number"
+                                        value={tyreData.medium.time}
+                                        onChange={(e) => setTyreData({ ...tyreData, medium: { ...tyreData.medium, time: parseFloat(e.target.value) } })}
+                                    />
+                                </div>
+                                <div className="input-group">
+                                    <label>Medium Loss: </label>
+                                    <input
+                                        type="number"
+                                        value={tyreData.medium.loss}
+                                        onChange={(e) => setTyreData({ ...tyreData, medium: { ...tyreData.medium, loss: parseFloat(e.target.value) } })}
+                                    />
+                                </div>
+                                <div className="input-group">
+                                    <label>Hard Time: </label>
+                                    <input
+                                        type="number"
+                                        value={tyreData.hard.time}
+                                        onChange={(e) => setTyreData({ ...tyreData, hard: { ...tyreData.hard, time: parseFloat(e.target.value) } })}
+                                    />
+                                </div>
+                                <div className="input-group">
+                                    <label>Hard Loss: </label>
+                                    <input
+                                        type="number"
+                                        value={tyreData.hard.loss}
+                                        onChange={(e) => setTyreData({ ...tyreData, hard: { ...tyreData.hard, loss: parseFloat(e.target.value) } })}
+                                    />
+                                </div>
+                            </>
+                        )}
+                        <div className="input-group">
+                            <label>Circuit: </label>
+                            <select
+                                value={circuit}
+                                onChange={(e) => setCircuit(e.target.value)}
+                            >
+                                <option value="">Select a circuit</option>
+                                {circuits.map((circuitKey, index) => (
+                                    <option key={index} value={circuitKey}>
+                                        {circuitMap[circuitKey]} {/* Mostrar el nombre amigable */}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="input-group">
+                            <label>Laps: </label>
+                            <input
+                                type="number"
+                                value={laps}
+                                onChange={(e) => setLaps(parseInt(e.target.value))}
+                            />
+                        </div>
+                        <div className="input-group">
+                            <label>Pit Time (seconds): </label>
+                            <input
+                                type="number"
+                                value={pitTime}
+                                onChange={(e) => setPitTime(parseInt(e.target.value))}
+                            />
+                        </div>
+                    </div>
+                    <button type="submit" className="submit-button">Submit</button>
+                </form>
+                {loading && <p className="loading-text">Estamos calculando la estrategia...</p>}
+                {responseText && (
+                    <div className="response-container">
+                        <h3>Response Data</h3>
+                        <pre>{responseText}</pre>
+                    </div>
+                )}
+            </div>
+
+            <Link to="/games" className="arrow-link">
+                <img src='https://api.iconify.design/prime:arrow-left.svg?color=red' alt="Arrow" className="arrow-img" />
+            </Link>
         </div>
     );
 }
